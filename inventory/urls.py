76bdfb64864d3b2eb67df638c725ef840b877e18
sleepy <132650214+krugstergaming@users.noreply.github.com>
2025-01-18 02:47:41 +0800

    may url na

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import ClassroomListView
+from .views import ClassroomListView, ProductListView
 
 urlpatterns = [
     path('', views.classroom_view, name='classroom'),
     path('inventory/', views.inventory_view, name='inventory'),
-    path('classroom/', ClassroomListView.as_view(), name="classroom_api")
+    path('classroom/', ClassroomListView.as_view(), name="classroom_api"),
+    path('product/', ProductListView.as_view(), name="product_api")
 ]
